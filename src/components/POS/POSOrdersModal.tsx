c154c93e57d0a2efbb88d2c6eadea8c47aa9d3da
abarratos4-spec <abarratos4@gmail.com>
2025-08-10 12:12:11 -0600

    Updated POSOrdersModal.tsx

--- src/components/POS/POSOrdersModal.tsx
+++ src/components/POS/POSOrdersModal.tsx
@@ -91,8 +91,8 @@ return (
       </div>
 
       {/* Orders Table */}
-      <div className="flex-1 overflow-hidden">
-        <div className="h-full overflow-y-auto overflow-x-auto custom-scrollbar">
+<div className="flex-1 overflow-y-auto">
+  <div className="min-h-[200px] overflow-x-auto custom-scrollbar">
         <table className="w-full text-xs sm:text-sm min-w-[600px]">
           <thead className="bg-gray-100 sticky top-0 border-b border-gray-200">
             <tr>
