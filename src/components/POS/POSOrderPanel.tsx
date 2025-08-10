9f7eafef1a36be1c9939364bd4ab0e1e38cef2b2
abarratos4-spec <abarratos4@gmail.com>
2025-08-10 17:12:27 -0600

    Updated POSOrderPanel.tsx

--- src/components/POS/POSOrderPanel.tsx
+++ src/components/POS/POSOrderPanel.tsx
@@ -220,7 +220,7 @@ export function POSOrderPanel({
                           setEditingItem(item);
                           setShowEditItemModal(true);
                         }}
-                        className="bg-orange-500 hover:bg-blue-700 text-white p-0.5 sm:p-1 rounded shadow-sm transition-colors"
+                        className="bg-orange-500 hover:bg-yellow-700 text-white p-0.5 sm:p-1 rounded shadow-sm transition-colors"
                         title="Editar"
                       >
                         <Edit size={8} className="sm:w-2.5 sm:h-2.5 lg:w-3 lg:h-3" />
