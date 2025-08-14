83d75e7168bbfe092fb84be04bd10f2dc61131a3
lamineyamal10oficialreal-pixel <lamineyamal10oficialreal@gmail.com>
2025-08-13 22:13:46 -0600

    Updated POSOrdersModal.tsx

--- src/components/POS/POSOrdersModal.tsx
+++ src/components/POS/POSOrdersModal.tsx
@@ -258,7 +258,7 @@ return (
     {showDetailModal && selectedOrderDetail && (
       <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-[60]">
         <div className="bg-white rounded-lg shadow-xl max-w-4xl w-full mx-4 max-h-[90vh] overflow-y-auto">
-          <div className="bg-gradient-to-r from-orange-600 to-red-600 p-4 border-b border-red-700 rounded-t-lg">
+          <div className=" bg-gradient-to-br from-orange-400 via-red-500 to-red-400 p-4 border-b border-red-700 rounded-t-lg">
             <div className="flex items-center justify-between">
               <h2 className="text-white font-bold text-xl">
                 Detalle del Pedido - #{selectedOrderDetail.id.slice(-6).toUpperCase()}
