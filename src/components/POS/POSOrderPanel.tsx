579bae3dabc1de7d31a5dc941403d71177cd84af
lamineyamal10oficialreal-pixel <lamineyamal10oficialreal@gmail.com>
2025-08-13 20:41:10 -0600

    Updated POSOrderPanel.tsx

--- src/components/POS/POSOrderPanel.tsx
+++ src/components/POS/POSOrderPanel.tsx
@@ -540,7 +540,7 @@ export function POSOrderPanel({
 )}
 
 
-      {/* Credit Authorization Modal */}
+      {/* Credit Authorization Modal 
       {showCreditAuthModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white rounded-lg shadow-xl max-w-md w-full mx-4">
@@ -617,6 +617,7 @@ export function POSOrderPanel({
           </div>
         </div>
       )}
+      */}
     </div>
   );
 }
