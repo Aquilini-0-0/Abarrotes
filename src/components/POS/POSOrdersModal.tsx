ef751af72ce5706facf8eb745b4c5f77a569100b
darwinlacabranunez-crypto <darwinlacabranunez@gmail.com>
2025-08-29 14:04:39 -0600

    Updated POSOrdersModal.tsx

--- src/components/POS/POSOrdersModal.tsx
+++ src/components/POS/POSOrdersModal.tsx
@@ -362,7 +362,7 @@ return (
     {showDetailModal && selectedOrderDetail && (
       <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-[60]">
         <div className="bg-white rounded-lg shadow-xl max-w-4xl w-full mx-4 max-h-[90vh] overflow-y-auto">
-          <div className=" bg-gradient-to-br from-orange-400 via-red-500 to-red-400 rounded-t-lg">
+          <div className=" bg-gradient-to-br from-orange-400 via-red-500 to-red-400 p-4 rounded-t-lg">
             <div className="flex items-center justify-between">
               <h2 className="text-white font-bold text-xl">
                 Detalle del Pedido - #{selectedOrderDetail.id.slice(-6).toUpperCase()}
